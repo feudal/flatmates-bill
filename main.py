0609ec246bc5b91867ad3e0682f285a8f439c005
calmis <calmis87@gmail.com>
2021-03-12 00:00:42 +0200

    Add FileSharer class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flats import Bill, Flatmate
-from reports import PdfReport
+from reports import PdfReport, FileSharer
 
 amount = float(input('Hey user, enter the bill amount: '))
 period = input('Enter the period. e.g. December 2021 ')
@@ -13,8 +13,11 @@ flatmate_name = input('Enter flatmate\'s name: ')
 days2 = int(input(f'Enter how many days {flatmate_name} was home in the bill period: '))
 flatmate2 = Flatmate(flatmate_name, days2)
 
-print(f'{flatmate1.name} pays: ',flatmate1.pays(the_bill, flatmate2))
-print(f'{flatmate2.name} pays: ',flatmate2.pays(the_bill, flatmate1))
+print(f'{flatmate1.name} pays: ', flatmate1.pays(the_bill, flatmate2))
+print(f'{flatmate2.name} pays: ', flatmate2.pays(the_bill, flatmate1))
 
 pdf_report = PdfReport(f'{the_bill.period}.pdf')
 pdf_report.generate(flatmate1, flatmate2, the_bill)
+
+file_sharer = FileSharer(filepath=pdf_report.filename)
+print(file_sharer.share())
--- reports.py
+++ reports.py
@@ -1,7 +1,7 @@
 import webbrowser
 import os
-
 from fpdf import FPDF
+from filestack import Client
 
 
 class PdfReport:
@@ -47,3 +47,15 @@ class PdfReport:
         pdf.output(self.filename)
 
         webbrowser.open(self.filename)
+
+
+class FileSharer:
+
+    def __init__(self, filepath, api_key='AF60AMuucQhiavHNX4dAiz'):
+        self.filepath=filepath
+        self.api_key=api_key
+
+    def share(self):
+        client = Client(self.api_key)
+        new_filelink = client.upload(filepath=self.filepath)
+        return new_filelink.url
